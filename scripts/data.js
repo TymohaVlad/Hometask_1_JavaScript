202a0bc71bcaa293cbd10bab41af030832234c82
Vladyslav Tymoshenko <vlados230761@gmail.com>
2023-07-28 08:45:10 +0300

    create files structure

---
+++ scripts/data.js
@@ -0,0 +1,39 @@
+// Создаем переменную с данными о заметках в виде массива объектов
+const notesData = [
+    {
+      id: 1,
+      createTime: '2023-07-28 10:00',
+      content: 'Сходить в магазин',
+      category: 'Задача',
+      dates: ['2023-07-28']
+    },
+    {
+      id: 2,
+      createTime: '2023-07-28 12:40',
+      content: 'Идея для проекта',
+      category: 'Идея',
+      dates: []
+    },
+    {
+        id: 3,
+        createTime: '2023-07-28 13:15',
+        content: 'Идея для проекта',
+        category: 'Идея',
+        dates: []
+      },
+      {
+        id: 4,
+        createTime: '2023-07-28 14:30',
+        content: 'Идея для проекта',
+        category: 'Идея',
+        dates: []
+      },
+      {
+        id: 5,
+        createTime: '2023-07-28 15:00',
+        content: 'Идея для проекта',
+        category: 'Идея',
+        dates: []
+      },
+  ];
+  
